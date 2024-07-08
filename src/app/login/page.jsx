1b21722d756ec90cb1b26ccd68007728ec6aed42
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-07-09 02:23:38 +0530

    commented login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,21 +1,21 @@
-import useLogin from "../../../utils/useLogin.js";
-import { redirect } from "next/navigation";
-import Link from "next/link";
+// import useLogin from "../../../utils/useLogin.js";
+// import { redirect } from "next/navigation";
+// import Link from "next/link";
 
 
 const Login = () => {
-    const { email, setEmail, password, setPassword, login, handleSubmit } = useLogin();
+    // const { email, setEmail, password, setPassword, login, handleSubmit } = useLogin();
     
 
 
-    if(login === true){
-        redirect('/');
-    }
+    // if(login === true){
+    //     redirect('/');
+    // }
 
     return(
         <div className="flex h-screen w-screen justify-center items-center bg-white text-black">
-            <div className='flex flex-col justify-center items-center bg-slate-200 px-5 py-10 rounded-md'>
-                {/* <h1 className='title'>AMUStudy</h1> */}
+            {/* <div className='flex flex-col justify-center items-center bg-slate-200 px-5 py-10 rounded-md'>
+                 <h1 className='title'>AMUStudy</h1>
                 <h3 className='text-[1.5rem] font-semibold mb-10'>Login</h3>
                 <form onSubmit={handleSubmit} className='flex flex-col items-center gap-5'>
                     <input 
@@ -38,7 +38,7 @@ const Login = () => {
                 </form>
                 <p className='text-slate-500 mt-5'>New to resolveX?</p>
                 <Link href="/signup" className='text-blue-700'> Sign up</Link>
-            </div>
+            </div> */}
         </div>
     )
 }
